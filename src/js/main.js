9826ee0e570434e945e8245c337188916b4f0094
Преподаватель (teacher) <teacher@stud.nwags.local>
2019-07-18 18:21:30 +0300

    first commit

---
+++ src/js/main.js
@@ -0,0 +1,11 @@
+let beginCount = document.getElementById('start');
+let allValueClasses = document.querySelectorAll(".budget-value, .level-value, .expenses-value, .optionalexpenses-value, .income-value, .monthsavings-value, .yearsavings-value");
+let inputFields = document.getElementsByClassName('expenses-item');
+let accept = document.getElementsByTagName('button')[0];
+let accept2 = document.getElementsByTagName('button')[1];
+let count = document.getElementsByTagName('button')[2];
+
+let income = document.querySelectorAll('#income, #savings, #sum, #percent, .year-value, .month-value, .day-value');
+
+
+
